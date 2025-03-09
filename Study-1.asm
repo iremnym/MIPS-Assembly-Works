14d90f5ceb7e038d03c6c7084f777ca35091dcbc
İrem Nayim <145321269+iremnym@users.noreply.github.com>
2025-03-09 19:14:52 +0300

    Add files via upload

---
+++ Study-1.asm
@@ -0,0 +1,24 @@
+.data
+msg1: .asciiz "Sayıyı giriniz:"
+
+.text
+.globl main
+main:
+addi $v0, $zero, 5
+syscall
+add $t0,$v0, $zero
+
+div $t1, $t0, 5
+mul $t2, $t1, 9
+add $t3, $t2, 32
+
+la $a0,msg1
+addi $v0, $zero, 4
+syscall
+
+add $a0, $t3, $zero
+addi $v0, $zero, 1
+syscall
+
+addi $v0, $zero, 10
+syscall
