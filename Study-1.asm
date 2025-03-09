af7a516d4d14ad29e9684d8e9aeffca3c37989a3
İrem Nayim <145321269+iremnym@users.noreply.github.com>
2025-03-09 19:17:24 +0300

    Update Study-1.asm
    An assembly language program that takes a temperature value in Celsius from the user and converts it to Fahrenheit

--- Study-1.asm
+++ Study-1.asm
@@ -1,10 +1,12 @@
 #An assembly language program that takes a temperature value in Celsius from the user and converts it to Fahrenheit
+
 .data
 msg1: .asciiz "Sayıyı giriniz:"
 
 .text
 .globl main
 main:
+
 addi $v0, $zero, 5
 syscall
 add $t0,$v0, $zero
